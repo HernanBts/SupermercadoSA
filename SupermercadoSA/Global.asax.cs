90f79e5a59d3e406cb478872f7444a48d6ef1000
Hernan <bort0@hotmail.com>
2018-12-19 19:12:09 -0300

    Add roles. Add Superuser automatic account with roles.

--- SupermercadoSA/Global.asax.cs
+++ SupermercadoSA/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SupermercadoSA.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -17,11 +20,86 @@ namespace SupermercadoSA
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<
                                         Models.SupermercadoSAContext,
                                         Migrations.Configuration>());
+            ApplicationDbContext db = new ApplicationDbContext();
+            CreateRoles(db);
+            CreateSuperUser(db);
+            AddPermisionsToSuperuser(db);
+            db.Dispose();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void AddPermisionsToSuperuser(ApplicationDbContext db)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            var user = userManager.FindByName("superuser@supersa.com");
+            if (!userManager.IsInRole(user.Id, "SuperUser"))
+            {
+                userManager.AddToRole(user.Id, "SuperUser");
+            }
+            if (!userManager.IsInRole(user.Id, "View"))
+            {
+                userManager.AddToRole(user.Id, "View");
+            }
+            if (!userManager.IsInRole(user.Id, "Create"))
+            {
+                userManager.AddToRole(user.Id, "Create");
+            }
+            if (!userManager.IsInRole(user.Id, "Edit"))
+            {
+                userManager.AddToRole(user.Id, "Edit");
+            }
+            if (!userManager.IsInRole(user.Id, "Delete"))
+            {
+                userManager.AddToRole(user.Id, "Delete");
+            }
+        }
+
+        private void CreateSuperUser(ApplicationDbContext db)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            var user = userManager.FindByName("superuser@supersa.com");
+            if (user == null)
+            {
+                user = new ApplicationUser
+                {
+                    UserName = "superuser@supersa.com",
+                    Email = "superuser@supersa.com"
+                };
+                userManager.Create(user, "Super_321");
+            }
+        }
+
+        private void CreateRoles(ApplicationDbContext db)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+            if (!roleManager.RoleExists("SuperUser"))
+            {
+                roleManager.Create(new IdentityRole("SuperUser"));
+            }
+            if (!roleManager.RoleExists("View"))
+            {
+                roleManager.Create(new IdentityRole("View"));
+            }
+            if (!roleManager.RoleExists("Create"))
+            {
+                roleManager.Create(new IdentityRole("Create"));
+            }
+            if (!roleManager.RoleExists("Delete"))
+            {
+                roleManager.Create(new IdentityRole("Delete"));
+            }
+            if (!roleManager.RoleExists("Edit"))
+            {
+                roleManager.Create(new IdentityRole("Edit"));
+            }
         }
     }
 }
